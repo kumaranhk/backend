adcb192d3d032c59e5057216b66281d3bee9ce70
hariharakumaran <hariharakumaran@visailabs.com>
2024-07-01 09:02:59 +0530

    added cloud db

--- db-utils/mongoose.js
+++ db-utils/mongoose.js
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
 
-const localUri = "mongodb://127.0.0.1:27017/pro";
+import dotenv from "dotenv";
+dotenv.config();
+
+const dbUserName = process.env.DB_USER_NAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbCluster = process.env.DB_CLUSTER;
+const dbName = process.env.DB_NAME;
+
+const cloudUri = `mongodb+srv://${dbUserName}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+// const localUri = "mongodb://127.0.0.1:27017/pro";
 
 const mongooseConnect = async () => {
   try {
-    await mongoose.connect(localUri);
+    await mongoose.connect(cloudUri);
     console.log("Mongoose Connection established");
   } catch (e) {
     console.log("Mongoose Connection error: " + e.message);
